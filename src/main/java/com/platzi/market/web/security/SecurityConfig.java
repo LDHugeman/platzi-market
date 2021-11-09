3ba1bbd4d21570c6e09a3e223cc550f7f83f5bf9
LDHugeman <luisdavid.vp@gmail.com>
2021-11-10 00:09:06 +0100

    Clase 37 resolve

--- src/main/java/com/platzi/market/web/security/SecurityConfig.java
+++ src/main/java/com/platzi/market/web/security/SecurityConfig.java
@@ -14,9 +14,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Autowired
     private PlatziUserDetailsService platziUserDetailsService;
 
+    @Autowired
     private JwtFilterRequest jwtFilterRequest;
 
     @Override
@@ -27,8 +29,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
-                .anyRequest().authenticated().and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .anyRequest().authenticated().and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
     }
